e6c2976eff464d2571662360b5eaf57376b821f3
Nandhini Murugan <nandz1407@gmail.com>
2025-07-25 20:44:20 +0530

    frontend and backend connected , authentication

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,12 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Nandhu@2004', 
-  database: 'blood_mate_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
+
 
 connection.connect((err) => {
   if (err) throw err;
